a3a95b7302e502af79f2ff259e39132008a465a4
Andrew Page <andrew.page32@gmail.com>
2017-01-27 11:39:30 -0500

    Adding files for multiplayer networking tutorial

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour {
+
+    public const int maxHealth = 100;
+    public bool destroyOnDeath;
+
+    [SyncVar(hook = "OnChangeHealth")]
+    public int currentHealth = maxHealth;
+
+    public RectTransform healthbar;
+
+    private NetworkStartPosition[] spawnPoints;
+
+    void Start()
+    {
+        if(isLocalPlayer)
+        {
+            spawnPoints = FindObjectsOfType<NetworkStartPosition>();
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (!isServer)
+        {
+            return;
+        }
+
+        currentHealth -= amount;
+
+        if (currentHealth <= 0)
+        {
+            if(destroyOnDeath)
+            {
+                Destroy(gameObject);
+            } else
+            {
+                currentHealth = maxHealth;
+                // called on the server but invoked on the clients
+                RpcRespawn();
+            }
+        }
+    }
+
+    public void OnChangeHealth(int currentHealth)
+    {
+        healthbar.sizeDelta = new Vector2(currentHealth, healthbar.sizeDelta.y);
+    }
+
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        if(isLocalPlayer)
+        {
+            // Set the spawn point to origin as a default
+            Vector3 spawnPoint = Vector3.zero;
+
+            // If there is a spawn point array and the array is not empty, picka  spawn point at random
+            if(spawnPoints != null && spawnPoints.Length > 0)
+            {
+                spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+            }
+
+            // Set the player's position to the chosen spawn point
+            transform.position = spawnPoint;
+        }
+    }
+
+}
